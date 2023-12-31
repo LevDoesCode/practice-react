3392c086d25a21d589bdedbe78c83cdab4094610
LevDoesCode <levdoescode@gmail.com>
2023-12-30 19:17:49 -0500

    Multi-line component

--- 01-learning/01-path1/04-starting/src/components/ExpenseItem.js
+++ 01-learning/01-path1/04-starting/src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-    return <h2>Expense item!</h2>;
+    return (
+        <div>
+            <div>August 8th, 2030</div>
+            <div>
+                <h2>Fire Insurance</h2>
+                <div>$234.33</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
