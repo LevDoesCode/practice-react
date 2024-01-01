80e669cfc03fd2095c5b12f5ed828de7560a7554
LevDoesCode <levdoescode@gmail.com>
2024-01-01 00:25:43 -0500

    Added key and imported css

--- 01-learning/01-path1/04-starting/src/components/Expenses.js
+++ 01-learning/01-path1/04-starting/src/components/Expenses.js
@@ -1,10 +1,11 @@
 import ExpenseItem4 from "./ExpenseItem4";
+import "./Expenses.css";
 
 function Expenses(props) {
     return (
         <div className="expenses">
             {props.expenses.map((expense) => (
-                <ExpenseItem4 expense={expense} />
+                <ExpenseItem4 key={expense.id} expense={expense} />
             ))}
         </div>
     );
