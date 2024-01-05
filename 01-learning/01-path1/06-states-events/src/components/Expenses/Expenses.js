3ce1dca3fc8337d9c8d1712b4b9019bf868c15a3
LevDoesCode <levdoescode@gmail.com>
2024-01-04 20:09:36 -0500

    Used the stateless version of ExpenseItem

--- 01-learning/01-path1/06-states-events/src/components/Expenses/Expenses.js
+++ 01-learning/01-path1/06-states-events/src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
+import ExpenseItem from "./ExpenseItem2";
 import ExpensesFilter from "../NewExpense/ExpensesFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
@@ -8,12 +8,24 @@ import "./Expenses.css";
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState("2024");
 
+    let filterInfoText = "";
+    if (filteredYear === "2023") {
+        filterInfoText = "2024, 2025 & 2026";
+    } else if (filteredYear === "2024") {
+        filterInfoText = "2023, 2025 & 2026";
+    } else if (filteredYear === "2025") {
+        filterInfoText = "2023, 2024 & 2026";
+    } else if (filteredYear === "2026") {
+        filterInfoText = "2023, 2024 & 2025";
+    }
+
     const filterYearHandler = (year) => {
         setFilteredYear(year);
     };
     return (
         <Card className="expenses">
             <ExpensesFilter selectedYear={filteredYear} onChangeFilterYear={filterYearHandler} />
+            <p>Data for years {filterInfoText} is hidden</p>
             <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
             <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
             <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
