3a53951040a82155fdb57b54d575ee37cbeff2f7
LevDoesCode <levdoescode@gmail.com>
2024-01-05 02:52:07 -0500

    Added 3 ways to apply filters to list of expenses

--- 01-learning/01-path1/07-lists-cond-content/src/components/Expenses/Expenses.js
+++ 01-learning/01-path1/07-lists-cond-content/src/components/Expenses/Expenses.js
@@ -15,13 +15,34 @@ const Expenses = (props) => {
         //setFilteredExpenses(props.items.filter((expense) => expense.date.getFullYear().toString() === selectedYear));
     };
 
+    let expensesContent = <p>No expenses found.</p>;
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+        ));
+    }
+
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                {filteredExpenses.length === 0 && <p>No expenses found.</p>}
                 {filteredExpenses.map((expense) => (
                     <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
                 ))}
+                {filteredExpenses.length === 0 ? (
+                    <p>No expenses found</p>
+                ) : (
+                    filteredExpenses.map((expense) => (
+                        <ExpenseItem
+                            key={expense.id}
+                            title={expense.title}
+                            amount={expense.amount}
+                            date={expense.date}
+                        />
+                    ))
+                )}
+                {expensesContent}
             </Card>
         </div>
     );
