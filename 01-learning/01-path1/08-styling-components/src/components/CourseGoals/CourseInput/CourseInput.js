35ab6178df8d525673ffca7eae85468fc72ae646
LevDoesCode <levdoescode@gmail.com>
2024-02-01 22:57:48 -0500

    Imported css module and added as class name with dynamic styling

--- 01-learning/01-path1/08-styling-components/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-learning/01-path1/08-styling-components/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 
 import Button from "../../UI/Button/Button2";
-import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
 
 const CourseInput = (props) => {
     const [enteredValue, setEnteredValue] = useState("");
@@ -35,7 +35,7 @@ const CourseInput = (props) => {
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <div className={`form-control ${isValid ? "" : "invalid_input"}`}>
+            <div className={`${styles["form-control"]} ${!isValid && styles.invalid_input}`}>
                 <label>Course Goal</label>
                 <input type="text" value={enteredValue} ref={inputRef} onChange={goalInputChangeHandler} />
             </div>
