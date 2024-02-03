d29679174176af8b51a2a1362bae46ab0cea9de8
LevDoesCode <levdoescode@gmail.com>
2024-02-02 23:20:00 -0500

    Fixed the id generation logic

--- 01-learning/01-path1/09-debugging-react/src/App.js
+++ 01-learning/01-path1/09-debugging-react/src/App.js
@@ -10,10 +10,13 @@ const App = () => {
         { text: "Finish the course!", id: "g2" },
     ]);
 
+    const [lastID, setLastID] = useState(2);
+
     const addGoalHandler = (enteredText) => {
         setCourseGoals((prevGoals) => {
             const updatedGoals = [...prevGoals];
-            updatedGoals.unshift({ text: enteredText, id: "goal1" });
+            updatedGoals.unshift({ text: enteredText, id: `g${lastID + 1}` });
+            setLastID((prevID) => prevID + 1);
             return updatedGoals;
         });
     };
