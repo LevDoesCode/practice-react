05ecf7ddac2eb678a178aca04f32e4f3cdfa6524
LevDoesCode <levdoescode@gmail.com>
2024-02-07 13:43:05 -0500

    Added event handler skeleton

--- 01-learning/01-path1/10-project1-inv-calc/src/components/Form/Form.js
+++ 01-learning/01-path1/10-project1-inv-calc/src/components/Form/Form.js
@@ -1,28 +1,58 @@
 const Form = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Form submitted");
+    };
+
+    const handleReset = (event) => {
+        event.preventDefault();
+        console.log("Form reset");
+    };
+
+    const handleInputChange = (inputID, value) => {
+        console.log(inputID, value);
+    };
+
     return (
-        <form className="form">
+        <form onSubmit={handleSubmit} className="form">
             <div className="input-group">
                 <p>
                     <label htmlFor="current-savings">Current Savings ($)</label>
-                    <input type="number" id="current-savings" />
+                    <input
+                        onChange={(event) => handleInputChange("current-savings", event.target.value)}
+                        type="number"
+                        id="current-savings"
+                    />
                 </p>
                 <p>
                     <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-                    <input type="number" id="yearly-contribution" />
+                    <input
+                        onChange={(event) => handleInputChange("yearly-contribution", event.target.value)}
+                        type="number"
+                        id="yearly-contribution"
+                    />
                 </p>
             </div>
             <div className="input-group">
                 <p>
                     <label htmlFor="expected-return">Expected Interest (%, per year)</label>
-                    <input type="number" id="expected-return" />
+                    <input
+                        onChange={(event) => handleInputChange("expected-return", event.target.value)}
+                        type="number"
+                        id="expected-return"
+                    />
                 </p>
                 <p>
                     <label htmlFor="duration">Investment Duration (years)</label>
-                    <input type="number" id="duration" />
+                    <input
+                        onChange={(event) => handleInputChange("duration", event.target.value)}
+                        type="number"
+                        id="duration"
+                    />
                 </p>
             </div>
             <p className="actions">
-                <button type="reset" className="buttonAlt">
+                <button onClick={handleReset} type="reset" className="buttonAlt">
                     Reset
                 </button>
                 <button type="submit" className="button">
