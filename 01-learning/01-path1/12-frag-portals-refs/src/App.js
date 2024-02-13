a783e01e8d4219e2f6319526e49966e54a8740c3
LevDoesCode <levdoescode@gmail.com>
2024-02-13 00:50:17 -0500

    Imported and used Wrapper component

--- 01-learning/01-path1/12-frag-portals-refs/src/App.js
+++ 01-learning/01-path1/12-frag-portals-refs/src/App.js
@@ -1,26 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import AddUser from './components/Users/AddUser';
-import UsersList from './components/Users/UsersList';
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+import Wrapper from "./components/Helpers/Wrapper";
 
 function App() {
-  const [usersList, setUsersList] = useState([]);
+    const [usersList, setUsersList] = useState([]);
 
-  const addUserHandler = (uName, uAge) => {
-    setUsersList((prevUsersList) => {
-      return [
-        ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
-      ];
-    });
-  };
+    const addUserHandler = (uName, uAge) => {
+        setUsersList((prevUsersList) => {
+            return [...prevUsersList, { name: uName, age: uAge, id: Math.random().toString() }];
+        });
+    };
 
-  return (
-    <div>
-      <AddUser onAddUser={addUserHandler} />
-      <UsersList users={usersList} />
-    </div>
-  );
+    return (
+        <Wrapper>
+            <AddUser onAddUser={addUserHandler} />
+            <UsersList users={usersList} />
+        </Wrapper>
+    );
 }
 
 export default App;
