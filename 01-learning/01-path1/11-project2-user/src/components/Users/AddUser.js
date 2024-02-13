16261c703b2eb39c4e38694a21fa98ba62acf851
LevDoesCode <levdoescode@gmail.com>
2024-02-12 23:00:53 -0500

    Imported CSS module and applied to custom component

--- 01-learning/01-path1/11-project2-user/src/components/Users/AddUser.js
+++ 01-learning/01-path1/11-project2-user/src/components/Users/AddUser.js
@@ -1,17 +1,21 @@
 import React from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
 const AddUser = () => {
     const handleAddUser = (event) => {
         event.preventDefault();
     };
     return (
-        <form onSubmit={handleAddUser}>
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" />
-            <label htmlFor="age">Age (Years)</label>
-            <input id="age" type="number" />
-            <button type="submit">Add User</button>
-        </form>
+        <Card className={classes.input}>
+            <form onSubmit={handleAddUser}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text" />
+                <label htmlFor="age">Age (Years)</label>
+                <input id="age" type="number" />
+                <button type="submit">Add User</button>
+            </form>
+        </Card>
     );
 };
 
