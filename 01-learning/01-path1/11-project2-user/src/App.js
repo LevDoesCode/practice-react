2c86be50991de93db4732f566e4f8a50a49e1a74
LevDoesCode <levdoescode@gmail.com>
2024-02-13 00:00:54 -0500

    Managed user list state and used a handler, passed as prop

--- 01-learning/01-path1/11-project2-user/src/App.js
+++ 01-learning/01-path1/11-project2-user/src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
 import UserList from "./components/Users/UserList";
 
 function App() {
+    const [userList, setUserList] = useState([]);
+
+    const handleAddUser = (username, age) => {
+        setUserList((prevUserList) => {
+            return [...prevUserList, { name: username, age: age, id: Math.random().toString() }];
+        });
+    };
+
     return (
         <div>
-            <AddUser />
-            <UserList users={[]} />
+            <AddUser onAddUser={handleAddUser} />
+            <UserList users={userList} />
         </div>
     );
 }
