87c9ded2b9bb6c62890b67b16af9ea27a4654989
LevDoesCode <levdoescode@gmail.com>
2024-02-12 23:19:25 -0500

    Added state management for age and username

--- 01-learning/01-path1/11-project2-user/src/components/Users/AddUser.js
+++ 01-learning/01-path1/11-project2-user/src/components/Users/AddUser.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 const AddUser = () => {
+    const [enteredUsername, setEnteredUsername] = useState("");
+    const [enteredAge, setEnteredAge] = useState("");
+
     const handleAddUser = (event) => {
         event.preventDefault();
+        console.log(enteredUsername, enteredAge);
     };
+
+    const handleChangeUsername = (event) => {
+        setEnteredUsername(event.target.value);
+    };
+
+    const handleChangeAge = (event) => {
+        setEnteredAge(event.target.value);
+    };
+
     return (
         <Card className={classes.input}>
             <form onSubmit={handleAddUser}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text" />
+                <input id="username" type="text" onChange={handleChangeUsername} />
                 <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number" />
+                <input id="age" type="number" onChange={handleChangeAge} />
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
