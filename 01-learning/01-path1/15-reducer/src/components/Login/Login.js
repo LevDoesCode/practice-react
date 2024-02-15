f02eb8290ce2cea0c88ed69bb9902e878cfc81a4
LevDoesCode <levdoescode@gmail.com>
2024-02-14 23:40:19 -0500

    Used reducer states with useEffect

--- 01-learning/01-path1/15-reducer/src/components/Login/Login.js
+++ 01-learning/01-path1/15-reducer/src/components/Login/Login.js
@@ -6,16 +6,6 @@ import Button from "../UI/Button/Button";
 
 const emailReducer = (state, action) => {
     if (action.type === "USER_INPUT") {
-        return { value: action.val, isValid: action.val.trim().length > 6 };
-    }
-    if (action.type === "INPUT_BLUR") {
-        return { value: state.value, isValid: state.value.trim().length > 6 };
-    }
-    return { value: "", isValid: false };
-};
-
-const passwordReducer = (state, action) => {
-    if (action.type === "USER_INPUT") {
         return { value: action.val, isValid: action.val.includes("@") };
     }
     if (action.type === "INPUT_BLUR") {
@@ -24,10 +14,20 @@ const passwordReducer = (state, action) => {
     return { value: "", isValid: false };
 };
 
+const passwordReducer = (state, action) => {
+    if (action.type === "USER_INPUT") {
+        return { value: action.val, isValid: action.val.trim().length > 6 };
+    }
+    if (action.type === "INPUT_BLUR") {
+        return { value: state.value, isValid: state.value.trim().length > 6 };
+    }
+    return { value: "", isValid: false };
+};
+
 const Login = (props) => {
-    // const [enteredEmail, setEnteredEmail] = useState("");
+    // const [enteredEmail, setEnteredEmail] = useState('');
     // const [emailIsValid, setEmailIsValid] = useState();
-    // const [enteredPassword, setEnteredPassword] = useState("");
+    // const [enteredPassword, setEnteredPassword] = useState('');
     // const [passwordIsValid, setPasswordIsValid] = useState();
     const [formIsValid, setFormIsValid] = useState(false);
 
@@ -35,7 +35,6 @@ const Login = (props) => {
         value: "",
         isValid: null,
     });
-
     const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
         value: "",
         isValid: null,
@@ -49,16 +48,33 @@ const Login = (props) => {
         };
     }, []);
 
+    const { isValid: emailIsValid } = emailState;
+    const { isValid: passwordIsValid } = passwordState;
+
+    useEffect(() => {
+        const identifier = setTimeout(() => {
+            console.log("Checking form validity!");
+            setFormIsValid(emailIsValid && passwordIsValid);
+        }, 500);
+
+        return () => {
+            console.log("CLEANUP");
+            clearTimeout(identifier);
+        };
+    }, [emailIsValid, passwordIsValid]);
+
     const emailChangeHandler = (event) => {
         dispatchEmail({ type: "USER_INPUT", val: event.target.value });
 
-        setFormIsValid(event.target.value.includes("@") && passwordState.isValid);
+        // setFormIsValid(
+        //   event.target.value.includes('@') && passwordState.isValid
+        // );
     };
 
     const passwordChangeHandler = (event) => {
         dispatchPassword({ type: "USER_INPUT", val: event.target.value });
 
-        setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
+        // setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
     };
 
     const validateEmailHandler = () => {
