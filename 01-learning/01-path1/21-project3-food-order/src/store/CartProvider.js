eef22cfe600cc942b153f9545a36987b11f8cae6
LevDoesCode <levdoescode@gmail.com>
2024-02-22 11:17:37 -0500

    CartProvider component which includes the context provider

---
+++ 01-learning/01-path1/21-project3-food-order/src/store/CartProvider.js
@@ -0,0 +1,17 @@
+import CartContext from "./cart-context";
+
+const CartProvider = (props) => {
+    const handleAddItemToCart = (item) => {};
+
+    const handleRemoveItemFromCart = (id) => {};
+
+    const cartContext = {
+        items: [],
+        totalAmount: 0,
+        addItem: handleAddItemToCart,
+        removeItem: handleRemoveItemFromCart,
+    };
+    return <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>;
+};
+
+export default CartProvider;
