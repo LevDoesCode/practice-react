54b76c470981b52d5bcb3dec37e7da2c29fdf050
LevDoesCode <levdoescode@gmail.com>
2024-02-21 23:48:05 -0500

    Handlers and component properties for the cart

--- 01-learning/01-path1/21-project3-food-order/src/App.js
+++ 01-learning/01-path1/21-project3-food-order/src/App.js
@@ -1,12 +1,24 @@
+import { useState } from "react";
+
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+    const [cartIsShown, setCartIsShown] = useState(false);
+
+    const handleShowCart = () => {
+        setCartIsShown(true);
+    };
+
+    const handleHideCart = () => {
+        setCartIsShown(false);
+    };
+
     return (
         <>
-            <Cart />
-            <Header />
+            {cartIsShown && <Cart onClose={handleHideCart} />}
+            <Header onClickCart={handleShowCart} />
             <main>
                 <Meals />
             </main>
