f4b6ed4900d02f44fde6684532c133ecdabe9499
LevDoesCode <levdoescode@gmail.com>
2024-02-21 23:49:54 -0500

    Properties for onClose/onClick

--- 01-learning/01-path1/21-project3-food-order/src/components/UI/Modal.js
+++ 01-learning/01-path1/21-project3-food-order/src/components/UI/Modal.js
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom";
 import classes from "./Modal.module.css";
 
 const Backdrop = (props) => {
-    return <div className={classes.backdrop} onClick={props.onClick} />;
+    return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 
 const ModalOverlay = (props) => {
@@ -18,7 +18,7 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
     return (
         <>
-            {ReactDOM.createPortal(<Backdrop onClick={props.onClose} />, portalElement)}
+            {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
             {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
         </>
     );
