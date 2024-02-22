effa17980c55c73aee8d6787bec6021d474aaec4
LevDoesCode <levdoescode@gmail.com>
2024-02-22 14:08:19 -0500

    Used the reducer to process the ADD action type

--- 01-learning/01-path1/21-project3-food-order/src/store/CartProvider.js
+++ 01-learning/01-path1/21-project3-food-order/src/store/CartProvider.js
@@ -1,13 +1,38 @@
+import { useReducer } from "react";
+
 import CartContext from "./cart-context";
 
-const CartProvider = (props) => {
-    const handleAddItemToCart = (item) => {};
+const defaultCartState = {
+    items: [],
+    totalAmount: 0,
+};
 
-    const handleRemoveItemFromCart = (id) => {};
+const cartReducer = (state, action) => {
+    if (action.type === "ADD") {
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount,
+        };
+    }
+    return defaultCartState;
+};
+
+const CartProvider = (props) => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+    const handleAddItemToCart = (item) => {
+        dispatchCartAction({ type: "ADD", item: item });
+    };
+
+    const handleRemoveItemFromCart = (id) => {
+        dispatchCartAction({ type: "REMOVE", id: id });
+    };
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: handleAddItemToCart,
         removeItem: handleRemoveItemFromCart,
     };
