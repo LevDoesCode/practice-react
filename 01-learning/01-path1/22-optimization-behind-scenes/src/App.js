3a293a827ab2763b006ee82c05215be1658889ef
LevDoesCode <levdoescode@gmail.com>
2024-02-24 03:05:33 -0500

    Conditional rendering

--- 01-learning/01-path1/22-optimization-behind-scenes/src/App.js
+++ 01-learning/01-path1/22-optimization-behind-scenes/src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import Button from "./components/UI/Button/Button";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Hi there!</h1>
-    </div>
-  );
+    const [showParagraph, setShowParagraph] = useState(false);
+    console.log("App running");
+
+    const handleToggleParagraph = () => {
+        setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    };
+
+    return (
+        <div className="app">
+            <h1>Hi there!</h1>
+            {showParagraph && <p>This is new!</p>}
+            <Button onClick={handleToggleParagraph}>Toggle Paragraph!</Button>
+        </div>
+    );
 }
 
 export default App;
