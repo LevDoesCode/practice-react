39a6d359664727e44b7abb8d6c05c9b7e9b1b01c
LevDoesCode <levdoescode@gmail.com>
2024-03-08 23:25:15 -0500

    Added dynamic content using curly braces {} in a component

--- 01-learning/02-path2/05-react-essentials/src/App.jsx
+++ 01-learning/02-path2/05-react-essentials/src/App.jsx
@@ -1,9 +1,19 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
 function Header() {
+    const randomIndex = genRandomInt(reactDescriptions.length);
+    const word = reactDescriptions[randomIndex];
+    console.log(randomIndex, word);
+
     return (
         <header>
             <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
             <h1>React Essentials</h1>
-            <p>Fundamental React concepts you will need for almost any app you are going to build!</p>
+            <p>{word} React concepts you will need for almost any app you are going to build!</p>
         </header>
     );
 }
