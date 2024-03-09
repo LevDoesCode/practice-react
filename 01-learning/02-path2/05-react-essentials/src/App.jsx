e4b76f4f67fe6afb5da920672a965c8b590c8c42
LevDoesCode <levdoescode@gmail.com>
2024-03-09 11:24:30 -0500

    Used props for image, title and description

--- 01-learning/02-path2/05-react-essentials/src/App.jsx
+++ 01-learning/02-path2/05-react-essentials/src/App.jsx
@@ -1,4 +1,6 @@
 import reactImg from "./assets/react-core-concepts.png";
+import componetsImg from "./assets/components.png";
+import stateImg from "./assets/state-mgmt.png";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -20,11 +22,46 @@ function Header() {
     );
 }
 
+function CoreConcept(props) {
+    return (
+        <li>
+            <img src={props.image} alt={props.title} />
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+        </li>
+    );
+}
+
 function App() {
     return (
         <div>
             <Header />
             <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept
+                            title="Components"
+                            description="The Core UI building block."
+                            image={componetsImg}
+                        />
+                        <CoreConcept
+                            title="Components"
+                            description="The Core UI building block."
+                            image={componetsImg}
+                        />
+                        <CoreConcept
+                            title="Components"
+                            description="The Core UI building block."
+                            image={componetsImg}
+                        />
+                        <CoreConcept
+                            title="Components"
+                            description="The Core UI building block."
+                            image={componetsImg}
+                        />
+                    </ul>
+                </section>
                 <h2>Time to get started!</h2>
             </main>
         </div>
