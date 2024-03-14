2ffeef595b939cae27d2483c22420c7a507622ff
LevDoesCode <levdoescode@gmail.com>
2024-03-13 23:48:15 -0500

    Added conditional components to edit player name

--- 01-learning/02-path2/07-project2-tic-tac-toe/src/components/Player.jsx
+++ 01-learning/02-path2/07-project2-tic-tac-toe/src/components/Player.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing((prevState) => {
+            return !prevState;
+        });
+    }
+
+    let playerBox = <span className="player-name">{name}</span>;
+    if (isEditing) {
+        playerBox = <input type="text" required />;
+    }
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {playerBox}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
         </li>
     );
 }
