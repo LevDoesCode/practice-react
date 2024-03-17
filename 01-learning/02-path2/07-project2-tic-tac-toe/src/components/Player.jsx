3c1ab593c8d9bc80d6249f69472c5e7511dd533a
LevDoesCode <levdoescode@gmail.com>
2024-03-16 23:56:03 -0500

    Added two way binding for input editing

--- 01-learning/02-path2/07-project2-tic-tac-toe/src/components/Player.jsx
+++ 01-learning/02-path2/07-project2-tic-tac-toe/src/components/Player.jsx
@@ -1,16 +1,31 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
     const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
+
+    const inputRef = useRef(null);
 
     function handleEditClick() {
         setIsEditing((prevState) => !prevState);
+        if (isEditing) {
+            inputRef.current.focus();
+        }
     }
 
-    let playerBox = <span className="player-name">{name}</span>;
+    function handleInputChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    function handleFocus(event) {
+        event.target.select();
+    }
+
+    let playerBox = <span className="player-name">{playerName}</span>;
     let btnCaption = "Edit";
+
     if (isEditing) {
-        playerBox = <input type="text" required value={name} onFocus={handleFocus} />;
+        playerBox = <input type="text" required value={playerName} onChange={handleInputChange} ref={inputRef} />;
         btnCaption = "Save";
     }
 
