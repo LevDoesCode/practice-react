54a08311b8276a2f0c99423d5c6d1530df52a7bb
LevDoesCode <levdoescode@gmail.com>
2024-03-24 22:30:18 -0500

    Added UserInput component

--- 01-learning/02-path2/08-project3-calculator/src/App.jsx
+++ 01-learning/02-path2/08-project3-calculator/src/App.jsx
@@ -1,9 +1,15 @@
 import Header from "./components/Header";
+import UserInput from "./components/UserInput";
 
 const title = "Investment Calculator";
 
 function App() {
-    return <Header title={title} />;
+    return (
+        <>
+            <Header title={title} />
+            <UserInput />
+        </>
+    );
 }
 
 export default App;
