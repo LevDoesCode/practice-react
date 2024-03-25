4f88660618dae892e846bbd423bd141ec3921d0e
LevDoesCode <levdoescode@gmail.com>
2024-03-24 22:43:28 -0500

    Managed userInput state, passed as props to UserInput and imported Results

--- 01-learning/02-path2/08-project3-calculator/src/App.jsx
+++ 01-learning/02-path2/08-project3-calculator/src/App.jsx
@@ -1,13 +1,34 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
+import Results from "./components/Results";
 
 const title = "Investment Calculator";
 
+const startValues = {
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+};
+
 function App() {
+    const [userInput, setUserInput] = useState(startValues);
+
+    function handleChange(inputIdentifier, newValue) {
+        setUserInput((prevUserInput) => {
+            return {
+                ...prevUserInput,
+                [inputIdentifier]: newValue,
+            };
+        });
+    }
+
     return (
         <>
             <Header title={title} />
-            <UserInput />
+            <UserInput userInput={userInput} onChange={handleChange} />
+            <Results input={userInput} />
         </>
     );
 }
