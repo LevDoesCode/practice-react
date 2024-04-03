ba04454affdf5836d3b7c1345321115579d4499b
LevDoesCode <levdoescode@gmail.com>
2024-04-02 23:44:32 -0500

    Added timer which works with Start and Stop

--- 01-learning/02-path2/15-refs-portals-countdown-game/src/components/TimerChallenge.jsx
+++ 01-learning/02-path2/15-refs-portals-countdown-game/src/components/TimerChallenge.jsx
@@ -1,26 +1,42 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+
+let timer;
 
 export default function TimerChallenge({ title, targetTime }) {
+    const goodTimer = useRef(null);
     const [timerStarted, setTimerStarted] = useState(false);
     const [timerExpired, setTimerExpired] = useState(false);
+    //const [timer, setTimer] = useState(null);
 
     function handleStart() {
-        setTimeout(() => {
+        // timer = setTimeout(() => {
+        //     setTimerExpired(true);
+        //     setTimerStarted(false);
+        // }, targetTime * 1000);
+        goodTimer.current = setTimeout(() => {
             setTimerExpired(true);
             setTimerStarted(false);
         }, targetTime * 1000);
+        setTimerExpired(false);
         setTimerStarted(true);
+    }
+
+    function handleStop() {
+        clearTimeout(goodTimer.current);
+        setTimerStarted(false);
     }
 
     return (
         <section className="challenge">
             <h2>{title}</h2>
-            {timerExpired && !timerStarted && <p>You Lost!</p>}
+            {timerExpired && <p>You Lost!</p>}
             <p className="challenge-time">
                 {targetTime} second{targetTime === 1 ? "" : "s"}
             </p>
             <p>
-                <button onClick={handleStart}>{timerStarted ? "Stop" : "Start"} Challenge</button>
+                <button onClick={timerStarted ? handleStop : handleStart}>
+                    {timerStarted ? "Stop" : "Start"} Challenge
+                </button>
             </p>
             <p className={timerStarted ? "active" : undefined}>
                 {timerStarted ? "Time is running..." : "Timer inactive"}
