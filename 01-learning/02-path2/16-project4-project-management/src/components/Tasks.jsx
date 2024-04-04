5c65b8df5526eeade1e4eae56f973d5ae0690b31
LevDoesCode <levdoescode@gmail.com>
2024-04-04 17:49:16 -0500

    Conditional render of tasks using tasks prop and selectedProjectId

--- 01-learning/02-path2/16-project4-project-management/src/components/Tasks.jsx
+++ 01-learning/02-path2/16-project4-project-management/src/components/Tasks.jsx
@@ -1,15 +1,29 @@
 import NewTask from "./NewTask";
 
-export default function Tasks() {
+export default function Tasks({ tasks, selectedProjectId, onAdd, onDelete }) {
     return (
         <section>
             <h2 className="text-2xl font-bold text-stone700 mb-4">Tasks</h2>
-            <NewTask />
-            <p className="text-stone-800 my-4">This project does not have any tasks yet.</p>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-            </ul>
+            <NewTask onAdd={onAdd} />
+            {tasks.length === 0 && <p className="text-stone-800 my-4">This project does not have any tasks yet.</p>}
+            {tasks.length > 0 && (
+                <ul className="p-4 mt-8 bg-stone-200 rounded-md ">
+                    {tasks.map(
+                        (task) =>
+                            task.projectId === selectedProjectId && (
+                                <li
+                                    key={task.id}
+                                    className="flex justify-between my-4"
+                                >
+                                    <span>{task.text}</span>
+                                    <button className="bg-stone-300 text-stone-700 hover:text-red-500 px-2 py-1 rounded-md">
+                                        Clear
+                                    </button>
+                                </li>
+                            )
+                    )}
+                </ul>
+            )}
         </section>
     );
 }
