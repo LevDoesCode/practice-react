0433f665d9ab6c4c46a7b3c267cd37a9d3c8a5aa
LevDoesCode <levdoescode@gmail.com>
2024-04-04 11:04:07 -0500

    Added styling and changed to Button component

--- 01-learning/02-path2/16-project4-project-management/src/components/Modal.jsx
+++ 01-learning/02-path2/16-project4-project-management/src/components/Modal.jsx
@@ -1,6 +1,7 @@
 import { createPortal } from "react-dom";
 import { forwardRef, useImperativeHandle } from "react";
 import { useRef } from "react";
+import Button from "./Button";
 
 export default forwardRef(function Modal({ children, buttonText }, ref) {
     const dialog = useRef(null);
@@ -12,10 +13,16 @@ export default forwardRef(function Modal({ children, buttonText }, ref) {
         };
     });
     return createPortal(
-        <dialog ref={dialog}>
+        <dialog
+            ref={dialog}
+            className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md"
+        >
             {children}
-            <form method="dialog">
-                <button>{buttonText}</button>
+            <form
+                method="dialog"
+                className="mt-4 text-right"
+            >
+                <Button>{buttonText}</Button>
             </form>
         </dialog>,
         document.getElementById("modal-root")
