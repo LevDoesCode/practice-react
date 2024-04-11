4738dc5d1aa8c2085dc83b5d21fb8cba2a0eabf0
LevDoesCode <levdoescode@gmail.com>
2024-04-10 21:30:10 -0500

    Removed incoming props and used context instead

--- 01-learning/02-path2/17-context-api-usereducer/src/components/Cart.jsx
+++ 01-learning/02-path2/17-context-api-usereducer/src/components/Cart.jsx
@@ -1,9 +1,8 @@
 import { useContext } from "react";
 import { CartContext } from "../store/shopping-cart-context";
 
-export default function Cart({ onUpdateItemQuantity }) {
-    const cartCtx = useContext(CartContext);
-    const { items } = useContext(CartContext);
+export default function Cart() {
+    const { items, updateQty } = useContext(CartContext);
 
     const totalPrice = items.reduce((acc, item) => acc + item.price * item.quantity, 0);
     const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
@@ -23,9 +22,9 @@ export default function Cart({ onUpdateItemQuantity }) {
                                     <span> ({formattedPrice})</span>
                                 </div>
                                 <div className="cart-item-actions">
-                                    <button onClick={() => onUpdateItemQuantity(item.id, -1)}>-</button>
+                                    <button onClick={() => updateQty(item.id, -1)}>-</button>
                                     <span>{item.quantity}</span>
-                                    <button onClick={() => onUpdateItemQuantity(item.id, 1)}>+</button>
+                                    <button onClick={() => updateQty(item.id, 1)}>+</button>
                                 </div>
                             </li>
                         );
