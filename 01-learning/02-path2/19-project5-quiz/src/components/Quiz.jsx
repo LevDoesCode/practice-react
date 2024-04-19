d933802ed68f9fec47dd121eadbbe7656246dff8
LevDoesCode <levdoescode@gmail.com>
2024-04-18 21:59:18 -0500

    Implemented Quiz with questions and answers

---
+++ 01-learning/02-path2/19-project5-quiz/src/components/Quiz.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import questions from "../questions";
+
+export default function Quiz() {
+    const [userAnswers, setUserAnswers] = useState([]);
+
+    const activeQuestionIndex = userAnswers.length;
+
+    function handleSelectAnswer(selectedAnswer) {
+        // setUserAnswers([...userAnswers, selectedAnswer]);
+        setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
+    }
+
+    return (
+        <div id="quiz">
+            <div id="question">
+                <h2>{questions[activeQuestionIndex].text}</h2>
+                <ul id="answers">
+                    {questions[activeQuestionIndex].answers.map((answer, index) => (
+                        <li key={index} className="answer">
+                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+}
