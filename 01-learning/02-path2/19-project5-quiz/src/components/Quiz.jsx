a1fec65c20a878e38ce88a322a5842625b22940e
LevDoesCode <levdoescode@gmail.com>
2024-04-21 01:45:33 -0500

    Added logic for answered, correct and wrong answers

--- 01-learning/02-path2/19-project5-quiz/src/components/Quiz.jsx
+++ 01-learning/02-path2/19-project5-quiz/src/components/Quiz.jsx
@@ -5,8 +5,9 @@ import QuestionTimer from "./QuestionTImer";
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
+    const [answerState, setAnswerState] = useState("");
 
-    const activeQuestionIndex = userAnswers.length;
+    const activeQuestionIndex = answerState === "" ? userAnswers.length : userAnswers.length - 1;
 
     if (activeQuestionIndex === questions.length) {
         return (
@@ -16,12 +17,28 @@ export default function Quiz() {
             </div>
         );
     }
+
     const shuffledAnswers = [...questions[activeQuestionIndex].answers].sort(() => Math.random() - 0.5);
 
-    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
-        // setUserAnswers([...userAnswers, selectedAnswer]);
-        setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
-    }, []);
+    const handleSelectAnswer = useCallback(
+        function handleSelectAnswer(selectedAnswer) {
+            // setUserAnswers([...userAnswers, selectedAnswer]);
+            setAnswerState("answered");
+            setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
+            setTimeout(() => {
+                if (selectedAnswer === questions[activeQuestionIndex].answers[0]) {
+                    setAnswerState("correct");
+                } else {
+                    setAnswerState("wrong");
+                }
+                setTimeout(() => {
+                    setAnswerState("");
+                }, 2000);
+                console.log(answerState);
+            }, 1000);
+        },
+        [activeQuestionIndex]
+    );
 
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
@@ -31,11 +48,23 @@ export default function Quiz() {
                 <QuestionTimer key={activeQuestionIndex} timeout={5000} onTimeout={handleSkipAnswer} />
                 <h2>{questions[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {shuffledAnswers.map((answer, index) => (
-                        <li key={index} className="answer">
-                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-                        </li>
-                    ))}
+                    {shuffledAnswers.map((answer, index) => {
+                        const isSlected = userAnswers[userAnswers.length - 1] === answer;
+                        let cssClass = "";
+                        if (answerState === "answered" && isSlected) {
+                            cssClass = "selected";
+                        }
+                        if ((answerState === "correct" || answerState === "wrong") && isSlected) {
+                            cssClass = "wrong";
+                        }
+                        return (
+                            <li key={index} className="answer">
+                                <button onClick={() => handleSelectAnswer(answer)} className={cssClass}>
+                                    {answer}
+                                </button>
+                            </li>
+                        );
+                    })}
                 </ul>
             </div>
         </div>
