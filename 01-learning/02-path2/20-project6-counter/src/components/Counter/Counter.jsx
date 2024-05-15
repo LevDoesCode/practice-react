ab90eb91b33ff4b1765d212a9cebabb0ee66a045
LevDoesCode <levdoescode@gmail.com>
2024-05-14 21:39:57 -0500

    Implemented an array state for the counter history

--- 01-learning/02-path2/20-project6-counter/src/components/Counter/Counter.jsx
+++ 01-learning/02-path2/20-project6-counter/src/components/Counter/Counter.jsx
@@ -4,6 +4,7 @@ import IconButton from "../UI/IconButton.jsx";
 import MinusIcon from "../UI/Icons/MinusIcon.jsx";
 import PlusIcon from "../UI/Icons/PlusIcon.jsx";
 import CounterOutput from "./CounterOutput.jsx";
+import CounterHistory from "./CounterHistory.jsx";
 import { log } from "../../log.js";
 
 function isPrime(number) {
@@ -56,4 +57,44 @@ const Counter = memo(function Counter({ initialCount }) {
     );
 });
 
-export default Counter;
+const Counter2 = memo(function Counter({ initialCount }) {
+    log("<Counter /> rendered", 1);
+
+    const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]);
+
+    // const [counter, setCounter] = useState(initialCount);
+    const [counterChanges, setCounterChanges] = useState([initialCount]);
+
+    const currentCounter = counterChanges.reduce((prevCounter, counterChange) => prevCounter + counterChange, 0);
+
+    const handleDecrement = useCallback(function handleDecrement() {
+        // setCounter((prevCounter) => prevCounter - 1);
+        setCounterChanges((prevCounterChanges) => [-1, ...prevCounterChanges]);
+    }, []);
+
+    const handleIncrement = useCallback(function handleIncrement() {
+        // setCounter((prevCounter) => prevCounter + 1);
+        setCounterChanges((prevCounterChanges) => [1, ...prevCounterChanges]);
+    }, []);
+
+    return (
+        <section className="counter">
+            <p className="counter-info">
+                The initial counter value was <strong>{initialCount}</strong>. It{" "}
+                <strong>is {initialCountIsPrime ? "a" : "not a"}</strong> prime number.
+            </p>
+            <p>
+                <IconButton icon={MinusIcon} onClick={handleDecrement}>
+                    Decrement
+                </IconButton>
+                <CounterOutput value={currentCounter} />
+                <IconButton icon={PlusIcon} onClick={handleIncrement}>
+                    Increment
+                </IconButton>
+            </p>
+            <CounterHistory history={counterChanges} />
+        </section>
+    );
+});
+
+export default Counter2;
